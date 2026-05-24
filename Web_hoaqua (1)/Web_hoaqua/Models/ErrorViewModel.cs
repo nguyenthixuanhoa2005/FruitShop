fce8fcb86b6b0a2eac24cb6576117ea70ec8ef50
Nguyễn Thị Xuân Hoa <hoantx.23810310027@epu.edu.vn>
2026-05-24 22:53:14 +0700

    delete

--- Web_hoaqua (1)/Web_hoaqua/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Web_hoaqua.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Web_hoaqua (1)/Web_hoaqua/wwwroot/Upload/README.txt
+++
@@ -1,9 +0,0 @@
-Dat anh banner vao thu muc nay voi cac ten sau:
-- banner-1.jpg
-- banner-2.jpg
-- banner-3.jpg
-
-Trang chu dang su dung cac duong dan:
-/Upload/banner-1.jpg
-/Upload/banner-2.jpg
-/Upload/banner-3.jpg
